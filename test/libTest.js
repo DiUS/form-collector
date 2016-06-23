f41cee9678e4723563522f48ec707022fc090284
antklim <antklim@gmail.com>
2016-06-23 17:01:51 +1000

    Library test inited

---
+++ test/libTest.js
@@ -0,0 +1,41 @@
+const assert = require('assert')
+
+
+describe('Library', () => {
+
+
+  describe('getForms', () => {
+
+    it('should return list of forms')
+    it('should support filtering')
+    it('should support pagination')
+    it('should throw error when database is not available')
+
+  })
+
+
+  describe('getFormById', () => {
+
+    it('should return form by id')
+    it('should return NULL when form not found by id')
+    it('should throw error when database is not available')
+
+  })
+
+
+  describe('validateForm', () => {
+
+    it('should successfully validate form')
+    // please add tests of validation rules bellow
+
+  })
+
+
+  describe('createForm', () => {
+
+    it('should successfully save form data and return form entity')
+    it('should throw error when database is not available')
+    it('should throw error when file storage (S3) is not available')
+
+  })
+})
