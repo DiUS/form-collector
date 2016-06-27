dd3162a9285f044cb8d3b687839334ff9073a979
antklim <antklim@gmail.com>
2016-06-28 09:57:39 +1000

    MongoDB on docker
    * docker-compose.yml added
    * MongoDB on docker container
    * MongoDB seed added and imported to container

--- test/data_sourceTest.js
+++ test/data_sourceTest.js
@@ -11,25 +11,20 @@ describe('Data source', () => {
 
   let sandbox = null
 
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create()
-  })
-
-  afterEach(() => {
-    sandbox.restore()
-    ds.disconnectDB()
-  })
+  beforeEach(() => sandbox = sinon.sandbox.create())
+  afterEach(() => sandbox.restore())
 
   describe('DataBase', () => {
 
 
     describe('connectDB and disconnectDB', () => {
 
+      beforeEach(() => ds.disconnectDB())
+
       it('should handle DB connection errors', (done) => {
         const connectionError = new Error('Connection error')
         sandbox.stub(DBClient, 'connect', (url, cb) => cb(connectionError))
 
-        ds.disconnectDB()
         ds.connectDB({}, (err) => {
           assert.deepEqual(err, connectionError)
           assert(!ds.getDB())
@@ -53,73 +48,75 @@ describe('Data source', () => {
     })
 
 
-    describe('findDB', () => {
+    describe('Data manipulation methods', function() {
 
-      beforeEach((done) => {
-        sandbox.stub(DBClient, 'connect', (url, cb) => cb(null, db))
-        ds.connectDB({}, done)
-      })
+      beforeEach(() => ds.setDB(db))
+      afterEach(() => ds.disconnectDB())
 
-      it('should return error when DB is not available', (done) => {
-        ds.disconnectDB()
-        ds.findDB('forms', {}, (err) => {
-          assert.deepEqual(err, error.DBNotAvailable)
-          done()
+      describe('findDB', () => {
+
+        it('should return error when DB is not available', (done) => {
+          ds.disconnectDB()
+          ds.findDB('forms', {}, (err) => {
+            assert.deepEqual(err, error.DBNotAvailable)
+            done()
+          })
+        })
+
+        it('should return error when collection not found in DB', (done) => {
+          ds.findDB('unknown', {}, (err) => {
+            assert.deepEqual(err, error.DBCollectionNotFound('unknown'))
+            done()
+          })
+        })
+
+        it('should call find method of collection', (done) => {
+          sandbox.spy(db.collections.forms, 'find')
+          ds.findDB('forms', {}, (err, forms) => {
+            assert.ifError(err)
+            assert(db.collections.forms.find.calledOnce)
+            assert(!_.isEmpty(forms))
+            done()
+          })
         })
       })
 
-      it('should return error when collection not found in DB', (done) => {
-        ds.findDB('unknown', {}, (err) => {
-          assert.deepEqual(err, error.DBCollectionNotFound('unknown'))
-          done()
+
+      describe('saveDB', () => {
+
+        it('should return error when DB is not available', (done) => {
+          ds.disconnectDB()
+          ds.saveDB('forms', {}, (err) => {
+            assert.deepEqual(err, error.DBNotAvailable)
+            done()
+          })
         })
-      })
 
-      it('should call find method of collection', (done) => {
-        sandbox.spy(db.collections.forms, 'find')
-        ds.findDB('forms', {}, (err, forms) => {
-          assert.ifError(err)
-          assert(db.collections.forms.find.calledOnce)
-          assert(!_.isEmpty(forms))
-          done()
+        it('should return error when collection not found in DB', (done) => {
+          ds.saveDB('unknown', {}, (err) => {
+            assert.deepEqual(err, error.DBCollectionNotFound('unknown'))
+            done()
+          })
         })
-      })
-    })
 
+        it('should insert document and return document data', (done) => {
+          const formMock = { firstName: 'Joe', lastName: 'Smith' }
+          sandbox.spy(db.collections.forms, 'insert')
+          ds.saveDB('forms', formMock, (err, newForm) => {
+            assert.ifError(err)
 
-    describe('saveDB', () => {
+            // formMock and newForm should refer to different Objects
+            assert.notEqual(newForm, formMock)
 
-      beforeEach((done) => {
-        sandbox.stub(DBClient, 'connect', (url, cb) => cb(null, db))
-        ds.connectDB({}, done)
-      })
+            // newForm should have _id field automatically generated
+            assert(newForm._id)
+            assert.equal(newForm.firstName, 'Joe')
+            assert.equal(newForm.lastName, 'Smith')
 
-      it('should return error when DB is not available', (done) => {
-        ds.disconnectDB()
-        ds.saveDB('forms', {}, (err) => {
-          assert.deepEqual(err, error.DBNotAvailable)
-          done()
-        })
-      })
+            assert(db.collections.forms.insert.calledOnce)
 
-      it('should return error when collection not found in DB', (done) => {
-        ds.saveDB('unknown', {}, (err) => {
-          assert.deepEqual(err, error.DBCollectionNotFound('unknown'))
-          done()
-        })
-      })
-
-      it('should insert document and return document data', (done) => {
-        const formMock = { firstName: 'Joe', lastName: 'Smith' }
-        ds.saveDB('forms', formMock, (err, newForm) => {
-          assert.ifError(err)
-          // formMock and newForm should refer to different Objects
-          assert.notEqual(newForm, formMock)
-          // newForm should have _id field automatically generated
-          assert(newForm._id)
-          assert.equal(newForm.firstName, 'Joe')
-          assert.equal(newForm.lastName, 'Smith')
-          done()
+            done()
+          })
         })
       })
     })
